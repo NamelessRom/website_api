d1521178ac0489e3c1159d588c7e286e1d767f66
Alexander Martinz <eviscerationls@gmail.com>
2015-04-29 18:16:22 +0200

    dynamically generate supported devices
      * vendors and devices are now in json format, which allows easier editing
        like adding new devices, changing supported version etc
    
    Change-Id: If7fad482d4e88549742b2f902c8df4208528f8ba
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

---
+++ public/js/angular/device.js
@@ -0,0 +1,98 @@
+/**
+ * Created by alex on 29.04.15.
+ */
+(function () {
+    var app = angular.module('device', []);
+
+    app.controller("DeviceController", function () {
+        var main = this;
+
+        main.deviceColumns = ["Codename", "Device", "n-1.0 (4.4.x)", "n-2.0 (5.0.x)", "n-2.1 (5.1.x)"];
+        main.vendors = [
+            {
+                "name": "Nexus",
+                "devices": [
+                    {
+                        "codename": "hammerhead",
+                        "model": "Nexus 5",
+                        "n_1_0": "true",
+                        "n_2_0": "true",
+                        "n_2_1": "true"
+                    },
+                    {
+                        "codename": "mako",
+                        "model": "Nexus 4",
+                        "n_1_0": "true",
+                        "n_2_0": "true",
+                        "n_2_1": "false"
+                    }
+                ]
+            },
+            {
+                "name": "OnePlus",
+                "devices": [
+                    {
+                        "codename": "bacon",
+                        "model": "OnePlus One",
+                        "n_1_0": "true",
+                        "n_2_0": "true",
+                        "n_2_1": "true"
+                    }
+                ]
+            },
+            {
+                "name": "OPPO",
+                "devices": [
+                    {
+                        "codename": "find7",
+                        "model": "OPPO Find 7",
+                        "n_1_0": "true",
+                        "n_2_0": "true",
+                        "n_2_1": "true"
+                    },
+                    {
+                        "codename": "n3",
+                        "model": "OPPO N3",
+                        "n_1_0": "false",
+                        "n_2_0": "true",
+                        "n_2_1": "true"
+                    }
+                ]
+            },
+            {
+                "name": "Samsung",
+                "devices": [
+                    {
+                        "codename": "i9300",
+                        "model": "Samsung Galaxy S3",
+                        "n_1_0": "true",
+                        "n_2_0": "true",
+                        "n_2_1": "false"
+                    },
+                    {
+                        "codename": "i9305",
+                        "model": "Samsung Galaxy S3 LTE",
+                        "n_1_0": "true",
+                        "n_2_0": "true",
+                        "n_2_1": "false"
+                    },
+                    {
+                        "codename": "n7100",
+                        "model": "Samsung Note 2",
+                        "n_1_0": "true",
+                        "n_2_0": "true",
+                        "n_2_1": "false"
+                    },
+                    {
+                        "codename": "t0lte",
+                        "model": "Samsung Note 2 LTE",
+                        "n_1_0": "true",
+                        "n_2_0": "true",
+                        "n_2_1": "false"
+                    }
+                ]
+            }
+        ];
+    });
+
+})();
